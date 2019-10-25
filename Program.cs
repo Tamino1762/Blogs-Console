2f45ed9b555bab17db3274f933f11ba39dbc1768
Tamino1762 <40547572+Tamino1762@users.noreply.github.com>
2019-10-25 13:00:51 -0500

    Finished Assignment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using BlogsConsole.Models;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace BlogsConsole
 {
@@ -11,28 +12,68 @@ namespace BlogsConsole
         public static void Main(string[] args)
         {
             logger.Info("Program started");
+            string choice;
+            var db = new BloggingContext();
+            Console.WriteLine("Would you like to\n1. Display all blogs\n2. Add a blog\n3. Create a post");
+            choice = Console.ReadLine();
             try
             {
-
+                switch (choice)
+                {
+                    case ("2"):
+                        
                 // Create and save a new Blog
                 Console.Write("Enter a name for a new Blog: ");
                 var name = Console.ReadLine();
+               
 
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
+                var blog = new Blog {Name = name};
                 db.AddBlog(blog);
                 logger.Info("Blog added - {name}", name);
+                break;
 
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
+                    case ("1"):
+
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
 
                 Console.WriteLine("All blogs in the database:");
                 foreach (var item in query)
                 {
                     Console.WriteLine(item.Name);
                 }
-            }
+
+                        break;
+                    case ("3"):
+                        string blogChoice;
+                        // Display all Blogs from the database
+                        var blogQuery = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in blogQuery)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                        
+                        Console.WriteLine("Which blog would you like to post to?");
+                        blogChoice = Console.ReadLine();
+                        db.Blogs.Where(b => b.Name == blogChoice);
+                        Console.WriteLine($"Adding post to {blogChoice}");
+                        //add a post
+                        Console.WriteLine("Title:");
+                        var title = Console.ReadLine();
+                        Console.WriteLine("Add your post!");
+                        var content = Console.ReadLine();
+                        var post = new Post { Title = title, Content = content};
+                        db.AddPost(post);
+                        logger.Info($"Post added {title}");
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid choice.");
+                        logger.Error("Invalid choice");
+                        break;
+            }//switch
+            }//try
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
